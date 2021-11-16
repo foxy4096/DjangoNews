1065a39a8732eae56ff3fb6816ad94b8ec7730f3
Aditya Priyadarshi <54215788+foxy4096@users.noreply.github.com>
2021-11-16 16:01:34 +0530

    Changed the code

--- DjangoNews/settings.py
+++ DjangoNews/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'news.apps.NewsConfig',
     'markdownx',
-    'django.contrib.humanize',
+    'markdownify.apps.MarkdownifyConfig',
 ]
 
 MIDDLEWARE = [
